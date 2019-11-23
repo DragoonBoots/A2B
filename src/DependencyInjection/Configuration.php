a1fc883298c1bbf2017015466eda887fce0cfba2
Dan Keenan <dk@dankeenan.org>
2019-11-23 11:44:57 -0500

    Fix Symfony deprecations

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -16,12 +16,16 @@ class Configuration implements ConfigurationInterface
     /**
      * Generates the configuration tree builder.
      *
-     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     * @return TreeBuilder The tree builder
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('a2b');
+        $treeBuilder = new TreeBuilder('a2b');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('a2b');
+        }
 
         // @formatter:off
         $rootNode
